d247b3cd5a35a9f7429d9922233faae1c89ceba2
Carlos Saura <carlos.saura@xing.com>
2018-11-22 19:08:55 +0100

    Improve vim and nvim setup

--- setup.sh
+++ setup.sh
@@ -6,7 +6,19 @@ if ! command -v brew > /dev/null; then
   exit
 fi
 
+echo "Setting brew"
+
 ln -s "$(pwd)"/.Brewfile ~/.Brewfile
 
 brew bundle --global
+
+
+echo "Setting vim"
+
+if [ ! -d ~/.vim ]; then
+  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+ln -s "$(pwd)"/vim/vimrc ~/.vimrc
+ln -s "$(pwd)"/vim/.nvim ~/.config/nvim/init.vim
 
